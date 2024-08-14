e59ed4d39dddd870442620a13817f8210049cfa9
mob <lsh954325@gmail.com>
2024-08-14 20:46:12 +0900

    테스트 스크립트 추가

---
+++ PuzzleGames/Assets/Scripts/Test/TestBase.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestBase : MonoBehaviour
+{
+    TestInputAction inputActions;
+
+    public virtual void Awake()
+    {
+        inputActions = new TestInputAction();
+    }
+
+    public virtual void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Test.Key1.performed += OnKey1;
+        inputActions.Test.Key2.performed += OnKey2;
+        inputActions.Test.Key3.performed += OnKey3;
+        inputActions.Test.Key4.performed += OnKey4;
+        inputActions.Test.Key5.performed += OnKey5;
+    }
+
+    public virtual void OnDisable()
+    {
+        inputActions.Test.Key5.performed += OnKey5;
+        inputActions.Test.Key4.performed += OnKey4;
+        inputActions.Test.Key3.performed += OnKey3;
+        inputActions.Test.Key2.performed += OnKey2;
+        inputActions.Test.Key1.performed += OnKey1;
+        inputActions.Disable();
+    }
+
+    public virtual void OnKey1(InputAction.CallbackContext context)
+    {
+        // 사용할 함수 작성
+    }
+    public virtual void OnKey2(InputAction.CallbackContext context)
+    {
+        // 사용할 함수 작성
+    }
+    public virtual void OnKey3(InputAction.CallbackContext context)
+    {
+        // 사용할 함수 작성
+    }
+    public virtual void OnKey4(InputAction.CallbackContext context)
+    {
+        // 사용할 함수 작성
+    }
+    public virtual void OnKey5(InputAction.CallbackContext context)
+    {
+        // 사용할 함수 작성
+    }
+}
